4466476010cd99ba44c3a256ba41356c3f0488ce
BaconZhang <1540480285@qq.com>
2020-05-12 16:07:07 +0800

    multi request

---
+++ deno/code/multirequest.ts
@@ -0,0 +1,27 @@
+type asyncFunc = () => Promise<unknown>;
+
+const result: unknown[] = [];
+
+async function multiRequest(asyncFuncs: asyncFunc[], max: number = 2) {
+  const promises = asyncFuncs.map((f) => () => f().then(res => {
+    result.push(res);
+    return res;
+  }))
+  const group = Array.from(
+    { length: max }, (_, i) => i
+  ).map(
+    i => promises.filter((_,j) => j % max === i)
+  ).map(
+    funcs => funcs.reduce((prev, current) => {
+      return prev.then(() => current());
+    }, Promise.resolve() as Promise<unknown>)
+  );
+
+  await Promise.all(group);
+  console.log(result);
+}
+
+const funcs = Array.from({ length: 50 }, (_, i) => () => new Promise(resolve => {
+  resolve(i);
+}));
+multiRequest(funcs, 5);
