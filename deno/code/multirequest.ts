b8bb30e393892a84f1a13cefe21a8d993c586c7f
BaconZhang <1540480285@qq.com>
2020-05-12 16:14:51 +0800

    fix

--- deno/code/multirequest.ts
+++ deno/code/multirequest.ts
@@ -7,21 +7,16 @@ async function multiRequest(asyncFuncs: asyncFunc[], max: number = 2) {
     result.push(res);
     return res;
   }))
-  const group = Array.from(
-    { length: max }, (_, i) => i
-  ).map(
-    i => promises.filter((_,j) => j % max === i)
-  ).map(
-    funcs => funcs.reduce((prev, current) => {
-      return prev.then(() => current());
-    }, Promise.resolve() as Promise<unknown>)
-  );
-
+  const group = Array.from({ length: max }, (_, i) => i)
+    .map(i => promises.filter((_,j) => j % max === i))
+    .map(funcs => funcs.reduce(
+        (prev, current) => prev.then(() => current()),
+        Promise.resolve() as Promise<unknown>
+      )
+    );
   await Promise.all(group);
   console.log(result);
 }
 
-const funcs = Array.from({ length: 50 }, (_, i) => () => new Promise(resolve => {
-  resolve(i);
-}));
+const funcs = Array.from({ length: 50 }, (_, i) => () => Promise.resolve(i));
 multiRequest(funcs, 5);
